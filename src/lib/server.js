3808f6ecee0e17a27dd255eb4a8ea57472e97636
jimzhan <jim.zhan@me.com>
2024-01-14 22:46:29 +0800

    init security server setup

--- src/lib/server.js
+++ src/lib/server.js
@@ -1,6 +1,25 @@
 import Fastify from 'fastify'
+import helmet from '@fastify/helmet'
+import swagger from '@fastify/swagger'
 import logger from './logger.js'
 
-export default Fastify({
-  logger
+const server = Fastify({ logger })
+
+server.register(helmet, {
+  noCache: true,
+  policy: 'same-origin',
+  contentSecurityPolicy: {
+    useDefaults: true,
+    directives: {
+      imgSrc: ["'self'", 'data:'],
+      scriptSrc: ["'self' 'unsafe-inline'"]
+    }
+  }
 })
+
+server.register(swagger, () => ({
+  routePrefix: '/docs',
+  exposeRoute: true
+}))
+
+export default server
